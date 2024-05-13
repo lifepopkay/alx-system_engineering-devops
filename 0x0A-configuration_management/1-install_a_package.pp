485f9586d2a75d7cd6f85b3e0715c71bdd1330d4
kayode popoola <75029917+lifepopkay@users.noreply.github.com>
2024-05-13 11:58:51 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # Installs flask, version 2.1.0
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
