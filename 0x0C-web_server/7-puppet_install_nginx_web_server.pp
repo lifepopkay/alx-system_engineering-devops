9fedc5bf127460a72a9b9927c47683124ea1a920
kayode popoola <75029917+lifepopkay@users.noreply.github.com>
2024-05-14 15:48:57 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,24 @@
-# Setup New Ubuntu server with nginx
-
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
-}
+# install and set up nginx
 
 package { 'nginx':
-	ensure => 'installed'
+  ensure => 'installed',
 }
 
-exec { 'Nginx HTTP':
-	provide => 'shell',
-	command => 'sudo ufw allow "Nginx HTTP"',
+file { '/var/www/html/index.html':
+  require => Package['nginx'],
+  content => 'Hello World!',
 }
 
-file { 'var/www/html/index.html':
-	require => Package['nginx'],
-	content => 'Hello World!'
+file_line { 'add-rewrite':
+  ensure  => 'present',
+  require => Package['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'root /var/www/html;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+  notify  => Service['nginx'],
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+service { 'nginx':
+  ensure  => running,
+  require => File_line['add-rewrite'],
 }
-
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
-}
-	
