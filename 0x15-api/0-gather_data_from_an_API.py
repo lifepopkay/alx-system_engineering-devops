4a58d5b8ba6ed0b39febb2a7ad4dcfd4f5c0ae4b
lifepopkay <popoolafolas@gmail.com>
2024-06-04 14:00:50 -0700

    task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,12 +11,21 @@ from sys import argv
 emp_id = argv[1]
 base_url = 'https://jsonplaceholder.typicode.com/todos'
 url_todo = f'{base_url}/?userId={emp_id}'
-url_name = f'{base_url}/users/{emp_id}'
+url_name = f'{base_url}/users/?id={emp_id}'
 
 # Get employee name
-emp_name = requests.get(url_name).json()
-print(emp_name)
+emp_name = requests.get(url_name).json().get('name')
 
 # Get todo list
 emp_todo = requests.get(url_todo).json()
-print(emp_todo)
+
+all_tasks = len(emp_todo)
+tasks = []
+for i in emp_todo:
+    if i['completed'] is True:
+        tasks.append(i['title'])
+ 
+done_task = len(tasks)
+print(f'Employee {emp_name} is done with ({done_task}/{all_tasks})')
+for i in tasks:
+    print(f'\t {i}')
