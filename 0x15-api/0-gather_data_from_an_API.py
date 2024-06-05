d3c8e92af4a6e5085cbab5df6673cf9653291d86
lifepopkay <popoolafolas@gmail.com>
2024-06-05 08:36:19 +0100

    task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,18 +7,25 @@
 import requests
 from sys import argv
 
-
+def to_dict(data):
+    new_list = {}
+    for item in data:
+        for key, val in item.items():
+            new_list[key] = val
+        return new_list
+            
+        
 def get_data(employee_id):
     base_url = 'https://jsonplaceholder.typicode.com/'
     url_todo = f'{base_url}todos/?userId={employee_id}'
     url_name = f'{base_url}/users/?id={employee_id}'
 
     # Get employee name
-    emp_name = requests.get(url_name).json()[0]['name']
+    emp_name = requests.get(url_name).json()
+    name = to_dict(emp_name).get('name')
 
     # Get todo list
     emp_todo = requests.get(url_todo).json()
-
     all_tasks = len(emp_todo)
     tasks = []
     for i in emp_todo:
@@ -26,9 +33,9 @@ def get_data(employee_id):
             tasks.append(i['title'])
 
     done_task = len(tasks)
-    print(f'Employee {emp_name} is done with tasks({done_task}/{all_tasks}):')
+    print(f'Employee {name} is done with tasks({done_task}/{all_tasks}):')
     for i in tasks:
-        print(f' \t{i}')
+        print(f'\t {i}')
 
 
 if __name__ == "__main__":
